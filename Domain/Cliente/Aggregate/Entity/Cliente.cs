b6ba55f712746ab362ea367eac1588237c97a928
Alex Santos <arkanjo.alex@gmail.com>
2020-09-15 13:52:37 -0300

    Entidades anemicas

---
+++ Domain/Cliente/Aggregate/Entity/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Cliente.Aggregate.Entity
+{
+	public class Cliente
+	{
+		public int Id { get; set; }
+
+		public string Nome { get; set; }
+		public string CPF { get; set; }
+		public DateTime DataNascimento { get; set; }
+		public int Idade { get; set; }
+		public string NomeMae { get; set; }
+		public string NomePai { get; set; }
+
+		public IList<Endereco> Enderecos { get; set; }
+		public IList<Telefone> Telefones { get; set; }
+	}
+}
---
+++ Domain/Cliente/Aggregate/Entity/Telefone.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Cliente.Aggregate.Entity
+{
+	public class Telefone
+	{
+		public int Id { get; set; }
+		public string Numero { get; set; }
+		public int Tipo { get; set; }
+	}
+}
