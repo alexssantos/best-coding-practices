6f0f3bc166d7e469a7fd353f99468ecf5c39d615
Alex Santos <arkanjo.alex@gmail.com>
2020-09-16 12:04:55 -0300

    add Controller na API para criar Cliente

---
+++ 1. DominiosRicosComDDD/MelhoresPraticasModelagemDDD/Controllers/ClienteController.cs
@@ -0,0 +1,35 @@
+﻿using ApplicationService.Cliente;
+using MelhoresPraticasModelagemDDD.ViewModel.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MelhoresPraticasModelagemDDD.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ClienteController : ControllerBase
+	{
+		private IClienteService ClienteService { get; set; }
+
+		public ClienteController(IClienteService clienteService)
+		{
+			this.ClienteService = clienteService;
+		}
+
+		[HttpPost]
+		public IActionResult Post([FromBody] ClienteRequest request)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			this.ClienteService.Create(
+				request.Nome,
+				request.CPF,
+				request.DataNascimento
+			);
+
+			return Ok();
+		}
+	}
+}
---
+++ 1. DominiosRicosComDDD/MelhoresPraticasModelagemDDD/ViewModel/Request/ClienteRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MelhoresPraticasModelagemDDD.ViewModel.Request
+{
+	public class ClienteRequest
+	{
+		[Required(ErrorMessage = "Campo Nome é Obrigatório")]
+		public string Nome { get; set; }
+
+		[Required(ErrorMessage = "Campo CPF é Obrigatório")]
+		public string CPF { get; set; }
+
+		[Required(ErrorMessage = "Campo DataNascimento é Obrigatório")]
+		public DateTime DataNascimento { get; set; }
+	}
+}
