4dbf387d4c8f54bd6eef4b69f0f3ee46d7f5c9ec
Alex Santos <arkanjo.alex@gmail.com>
2020-09-16 11:57:36 -0300

    ApplicationService Proj - Usando Factory para criar Cliente
    add Referencias no startup para injeção de dependencia do service e repository

---
+++ 1. DominiosRicosComDDD/ApplicatoinService/Cliente/ClienteService.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Cliente.Aggregate.Factory;
+using Domain.Cliente.Aggregate.Repository;
+using System;
+
+namespace ApplicationService.Cliente
+{
+	public class ClienteService : IClienteService
+	{
+		private IClientRepository Repository { get; set; }
+
+		public ClienteService(IClientRepository clienteRepository)
+		{
+			Repository = clienteRepository;
+		}
+
+		public void Create(string nome, string CPF, DateTime dtNascimento)
+		{
+			if (this.Repository.GetByCPF(CPF) != null)
+				throw new Exception($"Cliente já existe na base com este cpf {CPF}");
+
+			var cliente = ClienteFactory.Criar(nome, CPF, dtNascimento);
+
+			this.Repository.Save(cliente);
+		}
+	}
+}
---
+++ 1. DominiosRicosComDDD/ApplicatoinService/Cliente/IClienteService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ApplicationService.Cliente
+{
+	public interface IClienteService
+	{
+		void Create(string mome, string CPF, DateTime dtNascimento);
+	}
+}
