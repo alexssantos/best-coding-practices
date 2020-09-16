8b1b740b5eef3a735e16f5788d79591075caae7f
Alex Santos <arkanjo.alex@gmail.com>
2020-09-16 11:50:19 -0300

    Repository Pattern

---
+++ 1. DominiosRicosComDDD/Domain/Cliente/Aggregate/Repository/IClientRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.Cliente.Aggregate.Repository
+{
+	public interface IClientRepository
+	{
+		public Entity.Cliente Get(int id);
+		public List<Entity.Cliente> GetAll();
+		public void Save(Entity.Cliente cliente);
+		public Entity.Cliente GetByCPF(string CPF);
+	}
+}
---
+++ 1. DominiosRicosComDDD/Repository/Cliente/ClienteRepository.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Cliente.Aggregate.Repository;
+using Domain.Cliente.Aggregate.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repository.Cliente
+{
+	public class ClienteRepository : IClientRepository
+	{
+		private static IList<Domain.Cliente.Aggregate.Entity.Cliente> db = new List<Domain.Cliente.Aggregate.Entity.Cliente>();
+
+		public Domain.Cliente.Aggregate.Entity.Cliente Get(int id)
+		{
+			return db.FirstOrDefault(x => x.Id == id);
+		}
+
+		public List<Domain.Cliente.Aggregate.Entity.Cliente> GetAll()
+		{
+			return db.ToList();
+		}
+
+		public Domain.Cliente.Aggregate.Entity.Cliente GetByCPF(CPF CPF)
+		{
+			return db.FirstOrDefault(x => x.CPF == CPF);
+		}
+
+		public Domain.Cliente.Aggregate.Entity.Cliente GetByCPF(string CPF)
+		{
+			return db.FirstOrDefault(x => x.CPF.Valor == CPF);
+		}
+
+		public void Save(Domain.Cliente.Aggregate.Entity.Cliente cliente)
+		{
+			db.Add(cliente);
+		}
+
+		public void Update(Domain.Cliente.Aggregate.Entity.Cliente cliente)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
